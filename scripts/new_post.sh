f8922c2f78a6db52a3f1899be444a874e03c66c5
Mike Robinson <mike@lytmus.com>
2018-11-23 20:05:13 -0800

    projects, not project.  tighten up new_post script

--- scripts/new_post.sh
+++ scripts/new_post.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
 
+contents_subdirs=$(ls -F content | grep '/' | cut -d'/' -f1)
+
 filename=$(echo "${2}" | sed -e's/ /-/g')
 
-case "$1" in
-    blog)
-        hugo new blog/${filename}.md
-        subl content/blog/${filename}.md
-    ;;
-     
-    music)
-        hugo new music/${filename}.md
-        subl content/music/${filename}.md
-    ;;
-     
-    project)
-        hugo new project/${filename}.md
-        subl content/project/${filename}.md
-    ;;
-    *)
-        echo $"Usage: $0 [blog|music|project] \"Title of your post\""
-        exit 1
+if [[ "$contents" =~ "$1" ]]; then 
+    hugo new ${1}/${filename}.md
+    subl content/${1}/${filename}.md
+    exit 0
+else 
+    echo $"Usage: ${0} [${contents_subdirs}] \"Title of your post\""
+    exit 1
+fi
 
-esac
