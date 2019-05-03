32dfa2d1fe42bd0cc479bb018bcaef31dbba6e13
Mike Robinson <mrobinson@Mikes-MacBook-Pro.local>
2019-05-02 22:30:29 -0500

    Fix all the meh in the new_post script

--- scripts/new_post.sh
+++ scripts/new_post.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-contents_subdirs=$(ls -F content | grep '/' | cut -d'/' -f1)
+contents_subdirs=$(ls -F $(dirname ${0})/../content    \
+                   | grep '/'       \
+                   | cut -d'/' -f1  \
+                   | tr '\n' ' '    \
+                   | sed -e's/ $//')
 
 filename=$(echo "${2}" | sed -e's/ /-/g')
 
-if [[ "$contents" =~ "$1" ]]; then 
+if [[ "$contents_subdirs" =~ "$1" ]]; then
     hugo new ${1}/${filename}.md
     subl content/${1}/${filename}.md
     exit 0
-else 
-    echo $"Usage: ${0} [${contents_subdirs}] \"Title of your post\""
+else
+    echo $"Usage: ${0} [${contents_subdirs// /|}] \"Title of your post\""
     exit 1
 fi
 
